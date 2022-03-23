4659927901b31dfa1211e0cdf8969936668b886d
Peter Liljenroth <29121387+peterlil@users.noreply.github.com>
2022-03-23 13:17:17 +0100

    cleaned up some and made w10 basic

--- monitoring/win-perf-counters/sql-general-governance/Make-PerfmonTemplate.ps1
+++ monitoring/win-perf-counters/sql-general-governance/Make-PerfmonTemplate.ps1
@@ -1,14 +1,21 @@
-param ([string] $Servername, [string] $InstanceName = "", [string] $Template = "PerformanceMonitorTemplate_2016.xml")
+param ([string] $Servername, [string] $InstanceName = "", [string] $Template)
+
+if ([string]::IsNullOrEmpty($Servername))
+{
+	$Servername = $env:computername
+}
+
 $strarr = Get-Content $Template
-	for ( $i = 0; $i -lt $strarr.Length; $i++ )
+
+for ( $i = 0; $i -lt $strarr.Length; $i++ )
+{ 
+	$strarr[$i] = ($strarr[$i] -replace "#SERVER#", $ServerName); 
+	if($InstanceName.Length -ne 0 -and $InstanceName.ToLower() -ne "default") 
 	{ 
-		$strarr[$i] = ($strarr[$i] -replace "#SERVER#", $ServerName); 
-		if($InstanceName.Length -ne 0 -and $InstanceName.ToLower() -ne "default") 
-		{ 
-			if($strarr[$i].Contains("SQLServer:SSIS") -eq $false)
-			{
-				$strarr[$i] = ($strarr[$i] -replace "SQLServer:", ([string]::Format('MSSQL${0}:', $InstanceName))); 
-			}
-		} 
-	}
-	Set-Content -Path ($Servername + "-PerfmonTemplate.xml") -Value $strarr
+		if($strarr[$i].Contains("SQLServer:SSIS") -eq $false)
+		{
+			$strarr[$i] = ($strarr[$i] -replace "SQLServer:", ([string]::Format('MSSQL${0}:', $InstanceName))); 
+		}
+	} 
+}
+Set-Content -Path ($Servername + "-PerfmonTemplate.xml") -Value $strarr
--- monitoring/win-perf-counters/sql-general-governance/copy-scripts.ps1
+++ monitoring/win-perf-counters/sql-general-governance/copy-scripts.ps1
@@ -2,4 +2,4 @@ $destPath = Join-Path -Path (Get-ChildItem env:SystemDrive).Value -ChildPath "Pe
 Copy-Item zip-logs.ps1 $destPath
 
 $destPath = Join-Path -Path (Get-ChildItem env:SystemDrive).Value -ChildPath "PerfLogs\remove-old-logs.ps1"
-Copy-Item remove-old-logs.ps1 C:\PerfLogs\remove-old-logs.ps1
+Copy-Item remove-old-logs.ps1 $destPath
