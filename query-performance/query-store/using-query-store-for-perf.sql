e046b405418d089372c778281e87b7425082af3d
oldilcaman <peter.liljenroth@tietoevry.com>
2026-01-23 15:16:54 +0100

    add query for most logical reads

--- query-performance/query-store/using-query-store-for-perf.sql
+++ query-performance/query-store/using-query-store-for-perf.sql
@@ -60,6 +60,56 @@ ORDER BY
 	--rsi.start_time desc
 
 
+/*
+ * Use this query to:
+ *   - Find queries with most logical reads
+ */
+ 
+SELECT TOP 50 
+	  rs.runtime_stats_id
+	, p.plan_id
+	, q.query_id
+	, qt.query_text_id
+	, qt.query_sql_text
+	, rs.count_executions
+	/* Duration */
+	, rs.max_duration / 1000 AS max_duration_in_ms -- Use ms, seconds or minutes depending on your circumstances
+	, rs.avg_duration / 1000 AS avg_duration_in_ms -- Use ms, seconds or minutes depending on your circumstances
+	, ROUND(rs.min_duration / 1000, 0) AS min_duration_in_ms -- Use ms, seconds or minutes depending on your circumstances
+	--, rs.max_duration / 1000000 AS max_duration_in_seconds -- Use ms, seconds or minutes depending on your circumstances
+	--, rs.avg_duration / 1000000 AS avg_duration_in_seconds -- Use ms, seconds or minutes depending on your circumstances
+	--, ROUND(rs.min_duration / 1000000, 0) AS min_duration_in_seconds -- Use ms, seconds or minutes depending on your circumstances
+	--, rs.max_duration / 60000000 AS max_duration_in_minutes -- Use ms, seconds or minutes depending on your circumstances
+	--, ROUND(rs.avg_duration / 60000000, 0) AS avg_duration_in_minutes -- Use ms, seconds or minutes depending on your circumstances
+	--, rs.min_duration / 60000000 AS min_duration_in_minutes -- Use ms, seconds or minutes depending on your circumstances
+	/* Logical reads (no of 8-KB pages read) */
+	, rs.avg_logical_io_reads * 8 / 1024 AS avg_logical_io_reads_mb
+	, rs.last_logical_io_reads * 8 / 1024 AS last_logical_io_reads_mb
+	, rs.min_logical_io_reads * 8 / 1024 AS min_logical_io_reads_mb
+	, rs.max_logical_io_reads * 8 / 1024 AS max_logical_io_reads_mb
+	, rs.stdev_logical_io_reads * 8 / 1024 AS stdev_logical_io_reads_mb
+
+	, rs.max_rowcount, ROUND(rs.avg_rowcount, 0) AS avg_rowcount, rs.min_rowcount
+	, CONVERT(nvarchar(30), rsi.start_time, 120) as rsi_start_time
+	, CONVERT(nvarchar(30), rsi.end_time, 120) as rsi_endtime
+	, rs.last_execution_time
+	--, rs.*, rsi.*, p.*
+FROM sys.query_store_runtime_stats rs
+INNER JOIN sys.query_store_runtime_stats_interval rsi ON rs.runtime_stats_interval_id = rsi.runtime_stats_interval_id
+INNER JOIN sys.query_store_plan p ON rs.plan_id = p.plan_id
+INNER JOIN sys.query_store_query q ON p.query_id = q.query_id
+INNER JOIN sys.query_store_query_text qt ON q.query_text_id = qt.query_text_id
+WHERE 
+	execution_type = 0 -- regular execution (successfully finished)
+	--	AND q.query_id NOT IN (4524, 1058, 1521, 6911, 140/* 1520*/) -- Add/remove query ids here narrow down to the queries you are looking for
+	--AND q.query_id IN (15654) -- When you found the query, comment row above and select only that query here, and you will get the list of plans for that query
+	AND rsi.start_time > DATEADD(day, -7, GETDATE())
+	--AND rsi.start_time >= '2021-09-27 09:00:00.000' AND rsi.end_time <= '2021-09-27 10:00:00.000'
+ORDER BY 
+	rs.avg_logical_io_reads DESC
+	--rsi.start_time desc
+
+
 
 /*
  * Use this query to:
